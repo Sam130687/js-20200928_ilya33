99d62ca03cd6b4bc39dabffc6d0e934a6eb11ed3
Ilya-Burtsev <ilyaburtsev@icloud.com>
2020-10-04 14:21:25 +0300

    1. Была реализована функция для сортировки 2. Была реализована функция принимающая объект и произвольное количество строк а возвращать будет новый объект с перечисленными полями 2. Была реализована функция принимающая объект и произвольное количество строк а возвращать будет новый объект без перечисленными полями

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,15 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const arr = Object.entries(obj);
+  const arrNew = [];
+  const n = arr.length;
+  for (let i = 0; i < n; i++){
+    console.log(arr[i][0]);
+    console.log(fields.indexOf(arr[i][0]));
+    if (fields.indexOf(arr[i][0]) > -1){
+      arrNew.push(arr[i]);
+    }
+  }
+  return Object.fromEntries(arrNew);
 };
